282747b714f3b20bbdf2635f7b2afd3950035d00
Georgi <georgi.parapanov@gmail.com>
2015-10-26 14:59:44 +0000

    initial 1

---
+++ helloSelector.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<p>
+    <?php
+        $query=$_GET["query"];
+        $type=$_GET["type"];
+        echo "the query is ".$query."and the type is ".$type;
+    ?>
+</p>
+</body>
+</html>
