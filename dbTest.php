8d86bb8fc8d2df1385db6cc4e239b738049c6451
Georgi <georgi.parapanov@gmail.com>
2015-11-01 13:28:52 +0000

    initial 1

--- dbTest.php
+++ dbTest.php
@@ -21,17 +21,30 @@
         // create a SQL query as a string
 
         $sql_query = "SELECT * FROM marvelmovies WHERE title LIKE('X-Men%')";
+        $sql_query2 ="SELECT * FROM marvelmovies where yearReleased<2010";
+        $sql_query3="SELECT * FROM marvelmovies where notes like ('%Oscar%;')";
 
         // execute the SQL query
 
         $result = $db->query($sql_query);
+        $result2=$db->query($sql_query2);
+        $result3=$db->query($sql_query3);
 
         // iterate over $result object one $row at a time
         // use fetch_array() to return an associative array
 
         while($row = $result->fetch_array()){
            // print out fields from row of data
-           echo  $row['marvelMovieID']." ".$row['notes']." ".$row['productionStudio']." ".$row['title']." ".$row['yearReleased'] . "<br>";
+           echo  "xmen movies<br>".$row['marvelMovieID']." ".$row['notes']." ".$row['productionStudio']." ".$row['title']." ".$row['yearReleased'] . "<br>";
+           echo "movies before 2010<br>". $row['marvelMovieID']." ".$row['notes']." ".$row['productionStudio']." ".$row['title']." ".$row['yearReleased'] . "<br>";
+        }
+        while($row = $result2->fetch_array()){
+            // print out fields from row of data
+            echo "movies before 2010<br>". $row['marvelMovieID']." ".$row['notes']." ".$row['productionStudio']." ".$row['title']." ".$row['yearReleased'] . "<br>";
+        }
+        while($row = $result3->fetch_array()){
+            // print out fields from row of data
+            echo "movies with oscars<br>". $row['marvelMovieID']." ".$row['notes']." ".$row['productionStudio']." ".$row['title']." ".$row['yearReleased'] . "<br>";
         }
 
         $result->close();
